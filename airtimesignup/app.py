433d3fce91667fe46c0c2c97b2c194fd7e34769f
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-10-02 10:48:12 -0700

    add pages infrastructure and upgrade-toggle

--- airtimesignup/app.py
+++ airtimesignup/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 from airtimesignup.database import db_session
 from airtimesignup.checkvat import get_vat_info
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static')
 
 
 ## Database teardown
@@ -13,6 +13,11 @@ def shutdown_session(exception=None):
 @app.route("/")
 def show():
     return render_template('index.html')
+
+@app.route("/packages/<string:package>")
+def show_package(package):
+    return render_template('packages/{0}.html'.format(package))
+
 
 @app.route("/checkvat/<string:vat>")
 def checkvat(vat):
