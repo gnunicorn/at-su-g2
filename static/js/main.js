f2d0114ea1790c4a455905800869df621afa9249
Patricia Garcia <pat@patriciagarcia.me>
2013-11-28 15:44:18 +0100

    Add js for the currency selection

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,3 @@
-
-
 $(function() {
   // Show/hide info panel
   $('.info').hover(
@@ -9,5 +7,26 @@ $(function() {
     function(){
       $(this).siblings('.panel').addClass('hidden');
     }
-    );
+  );
+
+  // Hide the update button for currency change if javascript is enabled
+  $('.update-currency button').addClass('hidden');
+
+  $('select#currency').change(function(){
+    var selected = $(this).find('option:selected').val();
+    $('.price').each(function(){
+      if ($(this).hasClass(selected)){
+        $(this).removeClass('hidden');
+      } else {
+        $(this).addClass('hidden');
+      }
+    });
+    $('a.order.btn').each(function(){
+      if ($(this).hasClass(selected)){
+        $(this).removeClass('hidden');
+      } else {
+        $(this).addClass('hidden');
+      }
+    });
+  });
 });
